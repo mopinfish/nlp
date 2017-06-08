ccd6c2c55cbfa0978648f260d79fa0e6c9b539f1
otsukano <otsukanoboru@next-group.jp>
2017-06-08 14:24:07 +0900

    [nlp] add test cases templates

---
+++ README.md
@@ -0,0 +1,6 @@
+# nlp learning project
+
+## prepare pytest
+```
+$ sudo pip install pytest pytest-env
+```
---
+++ deep/test/test_a.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+from unittest import TestCase
+from os import path
+APP_ROOT = path.dirname( path.abspath( __file__ ) ) + "/../"
+MODULE_PATH = APP_ROOT + 'src/examples'
+
+##########################################
+# resolve expamples path for import 
+##########################################
+import sys
+sys.path.append(MODULE_PATH)
+print  MODULE_PATH
+import perceptron
+
+
+##########################################
+# below test cases
+##########################################
+def test_perceptron_AND1():
+  x1, x2 = 1, 1
+  expect = 1
+  assert perceptron.AND(x1, x2) == expect
+ 
+def test_perceptron_AND1():
+  x1, x2 = 0, 0
+  expect = 0
+  assert perceptron.AND(x1, x2) == expect
